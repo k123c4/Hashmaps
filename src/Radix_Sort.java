cc6919d40412e6e3e685dbd7afd604a3e2bcb271
k123c4 <kchau061906@gmail.com>
2026-03-27 20:33:32 -0500

    Question 5 Radix_Sort

--- src/Radix_Sort.java
+++ src/Radix_Sort.java
@@ -1,13 +1,62 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.*;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Radix_Sort{
+
+    public static void main(String[] args) {
+        String[] input = { "solo", "pupperino", "amaterasu", "amazon", "puppy", "hydra", "amazonia", "vueltiao"};
+
+        List<String> list = new ArrayList<>(Arrays.asList(input));
+        String[] s = list.toArray(new String[0]);
+
+        radixSort(s);
+
+        System.out.println(String.join(", ", s));
+    }
+
+    public static void radixSort(String[] arr) {
+        if (arr == null || arr.length == 0) return;
+
+
+        int maxLength = 0;
+        for (String str : arr) {
+            maxLength = Math.max(maxLength, str.length());
+        }
+
+        for (int d = maxLength - 1; d >= 0; d--) {
+            countingSortByCharacter(arr, d);
+        }
+    }
+
+    private static void countingSortByCharacter(String[] arr, int d) {
+        int n = arr.length;
+        String[] output = new String[n];
+
+        int[] count = new int[257];
+
+        for (int i = 0; i < n; i++) {
+            int charIndex = getCharIndex(arr[i], d);
+            count[charIndex + 1]++;
+        }
+
+        for (int i = 1; i < 257; i++) {
+            count[i] += count[i - 1];
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            int charIndex = getCharIndex(arr[i], d);
+            output[count[charIndex + 1] - 1] = arr[i];
+            count[charIndex + 1]--;
+        }
+
+
+        System.arraycopy(output, 0, arr, 0, n);
+    }
+
+
+    private static int getCharIndex(String s, int d) {
+        if (d >= s.length()) {
+            return -1;
+        }
+        return s.charAt(d);
     }
 }
